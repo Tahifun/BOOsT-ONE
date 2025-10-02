e2d3a35f421ee83d9ce26e2a4fbb3936d9f6ff7a
Tahifun <rene190178@gmail.com>
2025-10-02 21:32:52 +0200

    Update security.ts

--- backend/middleware/security.ts
+++ backend/middleware/security.ts
@@ -14,7 +14,7 @@ export const securityMiddleware = (isProduction: boolean) => {
         defaultSrc: ["'self'"],
         scriptSrc: [
           "'self'",
-          "'unsafe-inline'", // Nur wenn wirklich n�tig
+          "'unsafe-inline'", // Nur wenn wirklich ntig
           "https://cdnjs.cloudflare.com",
           "https://unpkg.com"
         ],
