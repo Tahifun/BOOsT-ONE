39579198cb30f4c52b16624c05340301dd6934b8
Tahifun <rene190178@gmail.com>
2025-10-02 22:29:37 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="vite/client" />
 
 // Hier definieren wir alle Frontend-relevanten Variablen,
-// die �ber Vite verf�gbar sein sollen.
-// Wichtig: Nur Variablen mit Pr�fix "VITE_" sind f�r das Frontend sichtbar.
+// die ber Vite verfgbar sein sollen.
+// Wichtig: Nur Variablen mit Prfix "VITE_" sind fr das Frontend sichtbar.
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE?: string;       // z. B. "http://localhost:4001"
-  readonly VITE_CLIENT_ORIGIN?: string;  // optional, falls im Frontend ben�tigt
+  readonly VITE_CLIENT_ORIGIN?: string;  // optional, falls im Frontend bentigt
   readonly VITE_STRIPE_PUB_KEY?: string; // Stripe Publishable Key (pk_test_...)
-  // weitere VITE_ Variablen hier hinzuf�gen wenn du sie brauchst
+  // weitere VITE_ Variablen hier hinzufgen wenn du sie brauchst
 }
 
 interface ImportMeta {
