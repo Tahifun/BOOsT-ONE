375a99763212359366ef53f377eb20e20207177f
Tahifun <rene190178@gmail.com>
2025-10-02 20:01:50 +0200

    Update Header.tsx

--- src/components/media/layout/Header.tsx
+++ src/components/media/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext';
-import { useSubscription } from '../../contexts/SubscriptionContext';
+import { useAuth } from '@contexts/AuthContext';
+import { useSubscription } from '@contexts/SubscriptionContext';
 import './Header.css';
 
 interface HeaderProps {
